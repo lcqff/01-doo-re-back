27d5b2f5ad7c6d69cef957015cfbfbd52a7d93ae
YeonHu Yim <71930280+lcqff@users.noreply.github.com>
2024-06-05 21:54:41 +0900

    feat: import HttpMethod

--- src/main/java/doore/config/CorsConfig.java
+++ src/main/java/doore/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package doore.config;
 
+import com.amazonaws.HttpMethod;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
