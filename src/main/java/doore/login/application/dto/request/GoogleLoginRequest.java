53c505b8007a8bb5ab95fea54f9e069eaa9c4818
YeonHu Yim <71930280+lcqff@users.noreply.github.com>
2024-09-05 11:35:25 +0900

    Feature/#211 로컬과 서버 양측에서 모두 로그인이 가능하도록 수정한다 (#212)
    * feat: 인가코드 전달 시 redirect_uri를 함께 받아온다.
    
    #211
    
    * chore: 서브모듈 수정
    
    * chore: snakeCase에서 camelCase로 변경한다.

--- src/main/java/doore/login/application/dto/request/GoogleLoginRequest.java
+++ src/main/java/doore/login/application/dto/request/GoogleLoginRequest.java
@@ -3,6 +3,8 @@ package doore.login.application.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record GoogleLoginRequest(
+        @NotNull(message = "리다이렉션 uri은 null일 수 없습니다.")
+        String redirectUri,
         @NotNull(message = "인가 코드는 null일 수 없습니다.")
         String code
 ) {
